ae7da419c97e9dd1f21b7a09234daaec1c4ef497
Jeet Gandhi <jeet.gandhi@zuhlke.com>
2021-05-03 12:47:19 +0800

    Initial Commit

---
+++ GraphQLSwiftUIDemo/GraphQLSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLSwiftUIDemoApp.swift
+//  GraphQLSwiftUIDemo
+//
+//  Created by Jeet Gandhi on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
